aedd9e8b801421ebfe8865a804ad3391fb66f954
ashis-solomon <ashissolomon24@gmail.com>
2023-04-23 20:57:14 +0530

    update

--- core/authentication/models.py
+++ core/authentication/models.py
@@ -6,20 +6,6 @@ class User(AbstractUser):
     rollno = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
-    # groups = models.ManyToManyField(
-    #     'auth.Group',
-    #     related_name='custom_user_groups',
-    #     blank=True,
-    #     help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-    #     verbose_name='groups',
-    # )
-    # user_permissions = models.ManyToManyField(
-    #     'auth.Permission',
-    #     related_name='custom_user_permissions',
-    #     blank=True,
-    #     help_text='Specific permissions for this user.',
-    #     verbose_name='user permissions',
-    # )
 
     def __str__(self):
         return self.username
