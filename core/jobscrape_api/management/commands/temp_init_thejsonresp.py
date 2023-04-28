2c6ac0b40d609528089e681a630c2eae8aa4932a
ashis-solomon <ashissolomon24@gmail.com>
2023-04-28 21:25:46 +0530

    modified json load command .. the command deletes the entries in ScrapeResults and loads it again from the /media/{jsonfile} ..... so when a cron job is run then the output will be like this json which is then loaded to the DB

--- core/jobscrape_api/management/commands/temp_init_thejsonresp.py
+++ core/jobscrape_api/management/commands/temp_init_thejsonresp.py
@@ -7,7 +7,9 @@ class Command(BaseCommand):
     help = 'temporarily initializes the ScrapeResult object'
 
     def handle(self, *args, **kwargs):
-        if ScrapeResult.objects.count() == 0:
+        try:
+            ScrapeResult.objects.all().delete()
+
             with open('./media/temp_json_response.json') as f:
                 data = json.load(f)
 
@@ -18,7 +20,22 @@ class Command(BaseCommand):
                 scrape_result.save()
 
             self.stdout.write(self.style.SUCCESS('TEMP ScrapeResult loaded successfully'))
-        else:
-            self.stdout.write(self.style.WARNING('TEMP ScrapeResult already exists, skipping data load'))
+        except:
+            self.stdout.write(self.style.ERROR('TEMP ScrapeResult loading FAILED!'))
+
+
+        # if ScrapeResult.objects.count() == 0:
+        #     with open('./media/temp_json_response.json') as f:
+        #         data = json.load(f)
+
+        #     for job_name in data.keys():
+        #         scrape_job = ScrapeJob.objects.get(job_name=job_name)
+        #         json_resp = {job_name: data[job_name]}
+        #         scrape_result = ScrapeResult(job_name=scrape_job, json_resp=json_resp, date_created=datetime.now())
+        #         scrape_result.save()
+
+        #     self.stdout.write(self.style.SUCCESS('TEMP ScrapeResult loaded successfully'))
+        # else:
+        #     self.stdout.write(self.style.WARNING('TEMP ScrapeResult already exists, skipping data load'))
 
     
